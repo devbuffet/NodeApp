94d681258881000c3c104844f934c95dd9e856e6
devbuffet <basharabuelone1@yahoo.com>
2015-12-07 15:14:46 -0500

    node app first commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,119 @@
+(function () {
+  var app = angular.module('movies', ['ui.bootstrap']);
+  
+  // get the data
+  app.controller('ListingCtrl', function ($scope, $http) {
+    
+    // movie data model
+    $scope.Movie = {
+      category: [
+        { key: 'recent', friendly_name: 'New Release' },
+        { key: 'featured', friendly_name: 'Featured' },
+        { key: 'recent', friendly_name: 'Recent' },
+        { key: 'recent_update', friendly_name: 'Recent (Update)' },
+        { key: 'top', friendly_name: 'Top' }
+      ],
+      movies: [],
+      selectedCategory: { key: null, friendly_name: 'Select Category' },
+      availableOptions: [
+        { id: 0, name: '50' },
+        { id: 1, name: '60' },
+        { id: 2, name: '70' },
+        { id: 3, name: '80' },
+        { id: 4, name: '90' }
+      ],
+      selectedOption: [],
+      listingProgress: null,
+      itemProgress: [],
+      movieMessage: null,
+      movieTitle: null
+    };
+    
+    // filter movie by title
+    $scope.filterMovie = function (resp) {
+
+      var movies = [];
+     
+      for (var i = 0; i < resp.length; i++) {
+        var item = resp[i];
+        if (item.title != undefined && item.title.toLowerCase().indexOf($scope.Movie.movieTitle) > -1) {
+          movies.push(item);
+        }
+      }
+      return movies;
+    };
+    
+    // get movie listings based on category
+    $scope.getMoviesListing = function () {
+      
+      // get selected category
+      var selectedCategory = $scope.Movie.selectedCategory;
+      
+      // show please wait...
+      $scope.Movie.movies = [];
+      $scope.Movie.listingProgress = true;
+      $scope.Movie.movieMessage = null;
+
+      $http.get('/movies/api/list/' + selectedCategory.key)
+        .success(function (resp) {
+          
+          // set movie data
+          $scope.Movie.movies = ($scope.Movie.movieTitle == null || $scope.Movie.movieTitle.length == 0 ? resp : $scope.filterMovie(resp));
+          
+          // movie message
+          $scope.Movie.movieMessage = $scope.Movie.movies.length + " results found for  " + selectedCategory.friendly_name;
+          
+          // default the rating to 50%          
+          $scope.setRating(0, $scope.Movie.movies.length);
+                    
+          // clear please wait
+          $scope.Movie.listingProgress = null;
+        });
+    };
+    
+    // defaults the rating
+    $scope.setRating = function (indx, count) {
+      // default rating is 50%
+      for (var i = 0; i < count; i++) {
+        $scope.Movie.selectedOption[i] = $scope.Movie.availableOptions[indx];
+      }
+    };
+    
+    // toggles category
+    $scope.toggleCategory = function (item) {
+      $scope.Movie.selectedCategory = item;
+    };
+    
+    // get filtered rating
+    $scope.getFilteredRating = function (index) {
+
+      var threshold = $scope.Movie.selectedOption[index].name;
+        
+      // show please wait...
+      $scope.Movie.itemProgress[index] = 'please wait...';
+
+      $scope.Movie.movies[index].data.rating = null;
+
+      $http.get('/movies/api/show/' + $scope.Movie.movies[index].key + '/' + threshold)
+        .success(function (resp) {
+
+          $scope.Movie.movies[index].data.rating = resp;
+
+          if (resp.length > 0) {
+            // hide message
+            $scope.Movie.itemProgress[index] = null;
+          }
+          else {
+            $scope.Movie.itemProgress[index] = 'No data found';
+          }
+        });
+    };
+  });
+  
+  // next controller...
+
+} ());
+
+
+
+
---
+++ routes/movies.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+
+var cheerio = require('cheerio');
+var request = require('request');
+var uniqueBy = require('unique-by');
+
+var objMsg;
+
+// get listings
+router.get('/api/list/:type', function (req, res) {
+  // start fresh
+  objData = [];
+  // get the html
+  request('http://www.zmovie.tw/movies/' + req.params.type, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      // load the html    
+      var $ = cheerio.load(html);
+      // get 'a' tags
+      $('a').each(function (i, element) {
+        // find the appropriate 'a' tag
+        if ($(this).attr('href') != undefined && $(this).attr('href').indexOf("http://www.zmovie.tw/movies/view") > -1) {
+          if ($(this).text().replace(/\s/g, "")) {
+            
+            // get the key to pass into record page
+            var key = $(this).attr('href').split('/');
+            
+            // clean title
+            var title = ($(this).attr('title') != undefined ? $(this).attr('title').replace('Watch','').replace('online','') : $(this).attr('title'));
+            
+            // add data to array
+            objData.push({ key: key[key.length - 1], href: $(this).attr('href'), title: title, image_url: "http://www.zmovie.tw/files/movies/" + $(this).attr('href').split('/')[5] + ".jpg", data: {rating:null} });
+          }
+        }
+      });
+    }
+    // return unique listing
+    res.json(uniqueBy(objData, 'key'));
+  });
+});
+
+// get the record
+router.get('/api/show/:movie/:rating', function (req, res) {
+  // start fresh
+  arr = [];
+  request('http://www.zmovie.tw/movies/view/' + req.params.movie, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+             
+      // get list of ids....  
+      var pattern = /mgid_[0-9]+/gm;
+      var id_match = $.html().match(pattern);
+
+      for (var i = 0; i < id_match.length; i++) {
+        var content = $('#' + id_match[i]).html();
+
+        var rating_match = content.match(/([0-9]+% said good)/g);
+        
+        // we are interested in ratings >= req.params.movie
+        if (rating_match.length > 0 && parseInt(rating_match[0]) >= req.params.rating) {
+         
+          // init array
+          var arr_link = { href: '', title: '', rating: 0 };
+         
+          // get the links...
+          $('#' + id_match[i]).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().find('a').each(function (x, y) {
+
+            var movie_link = $(y).attr('href');
+
+            if (movie_link != undefined && movie_link.indexOf('zmovie') == -1 && movie_link.indexOf('javascript') == -1 && movie_link.indexOf('#') == -1) {
+         
+              // set the attributes
+              arr_link.href = $(y).attr('href');
+              arr_link.title = $("title").text();
+              arr_link.rating = parseInt(rating_match[0]);
+            }
+          });
+         
+          // push data to array
+          arr.push({ data: arr_link });
+        }
+      }
+      
+      // sort the array in descending order based on rating
+      arr.sort(function (a, b) {
+        return parseInt(a.data.rating) < parseInt(b.data.rating);
+      });
+      
+    }
+    res.json(arr);
+  });
+});
+
+module.exports = router;
