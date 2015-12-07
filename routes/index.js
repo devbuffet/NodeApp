83e084bb14f2a467aca6663fe2cd667ae2d665b6
devbuffet <basharabuelone1@yahoo.com>
2015-12-07 16:59:57 -0500

    fixed api key

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Test' });
-});
-
 router.get('/user/auth/:api_key', function(req, res, next) {
-  res.json({api_key: process.env.api_key});
+  res.json({api_key: process.env.api_key}); 
 });
 
 module.exports = router;
